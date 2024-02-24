5846229632f2a00cdb35765162641f309afbdb9e
RikThePixel <rikdenbreejen@tutanota.com>
2024-02-24 14:54:49 +0100

    Add navigation commands

--- modules/navigation.lua
+++ modules/navigation.lua
@@ -1,4 +1,48 @@
+local function toggle_file_mark()
+    local harpoon = require("harpoon")
+    local harpoon_list = harpoon:list()
+    local original_length = harpoon_list:length()
+    harpoon_list:append()
+    if harpoon_list:length() ~= original_length then
+        return
+    end
+    harpoon_list:remove()
+    harpoon:sync()
+end
+
+local function toggle_quick_menu()
+    local harpoon = require("harpoon")
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+end
+
 
 return {
-    
+    {
+        'nvim-telescope/telescope.nvim',
+        keys = {
+            { "<leader>sf", function() require("telescope.builtin").find_files() end, desc = "[S]earch [F]iles" },
+            { "<leader>sg", function() require("telescope.builtin").live_grep() end,  desc = "[S]earch [G]rep" },
+            { "<leader>sr", function() require("telescope.builtin").git_files() end,  desc = "[S]earch [R]epository" },
+        },
+        cmd = { "Telescope" },
+        tag = '0.1.5',
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons"
+        },
+    },
+    {
+        'ThePrimeagen/harpoon',
+        branch = "harpoon2",
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+        },
+        opts = {},
+        keys = {
+            { "<leader>fm", toggle_file_mark,                                desc = "[F]ile [M]ark" },
+            { "<leader>fq", toggle_quick_menu,                               desc = "[F]ile [Q]uick menu" },
+            { "<leader>fn", function() require("harpoon"):list():next() end, desc = "[F]ile [N]ext" },
+            { "<leader>fp", function() require("harpoon"):list():prev() end, desc = "[F]ile [P]revious" },
+        },
+    }
 }
--- modules/theme.lua
+++ modules/theme.lua
@@ -1,27 +1,52 @@
+local function harpoon_mark()
+    local harpoon = require("harpoon")
+    if not harpoon then return "" end
+
+    local harpoon_list = harpoon:list()
+    local display = harpoon_list.config.create_list_item(harpoon_list.config).value
+    if display == nil or display == "" then
+        return ""
+    end
+
+    for i = 1, harpoon_list:length() do
+        if (harpoon_list:get(i).value == display) then
+            return "󰙒 " .. i
+        end
+    end
+    return ""
+end
+
 return {
     {
         "catppuccin/nvim",
-        name = "catppuccin", 
+        name = "catppuccin",
         priority = 1000,
         lazy = false,
         opts = {
             no_italic = true,
             integration = {
-                mason = true
+                mason = true,
+                harpoon = true
             }
         },
-        config = function(_,opts)
-            require("catppuccin").setup(opts)
-            vim.cmd([[colorscheme catppuccin-mocha]])
+        init = function()
+            vim.cmd("colorscheme catppuccin-mocha")
         end
     },
     {
         "nvim-lualine/lualine.nvim",
         lazy = false,
         -- Lualine theme configured by Catppuccin
-        opts = { },
-        dependencies = { 
-            { "nvim-tree/nvim-web-devicons", lazy = false }
+        opts = {
+            sections = {
+                lualine_c = {
+                    "filename",
+                    harpoon_mark
+                }
+            }
+        },
+        dependencies = {
+            { "nvim-tree/nvim-web-devicons" }
         }
     }
 }
