e8a15fb36c3ac2127ccfe0a973137fb1963e21a5
RikThePixel <rikdenbreejen@tutanota.com>
2024-02-24 17:08:28 +0100

    Style telescope

--- modules/navigation.lua
+++ modules/navigation.lua
@@ -18,20 +18,6 @@ end
 
 return {
     {
-        'nvim-telescope/telescope.nvim',
-        keys = {
-            { "<leader>sf", function() require("telescope.builtin").find_files() end, desc = "[S]earch [F]iles" },
-            { "<leader>sg", function() require("telescope.builtin").live_grep() end,  desc = "[S]earch [G]rep" },
-            { "<leader>sr", function() require("telescope.builtin").git_files() end,  desc = "[S]earch [R]epository" },
-        },
-        cmd = { "Telescope" },
-        tag = '0.1.5',
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons"
-        },
-    },
-    {
         'ThePrimeagen/harpoon',
         branch = "harpoon2",
         dependencies = {
@@ -44,5 +30,58 @@ return {
             { "<leader>fn", function() require("harpoon"):list():next() end, desc = "[F]ile [N]ext" },
             { "<leader>fp", function() require("harpoon"):list():prev() end, desc = "[F]ile [P]revious" },
         },
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        keys = {
+            { "<leader>sf", function() require("telescope.builtin").find_files() end, desc = "[S]earch [F]iles" },
+            { "<leader>sg", function() require("telescope.builtin").live_grep() end,  desc = "[S]earch [G]rep" },
+            { "<leader>sr", function() require("telescope.builtin").git_files() end,  desc = "[S]earch [R]epository" },
+        },
+        cmd = { "Telescope" },
+        tag = '0.1.5',
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons"
+        },
+        opts = function()
+            return {
+                defaults = {
+                    vimgrep_arguments = {
+                        "rg",
+                        "-L",
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--smart-case",
+                    },
+                    prompt_prefix = "   ",
+                    selection_caret = "  ",
+                    entry_prefix = "  ",
+                    initial_mode = "insert",
+                    layout_config = {
+                        horizontal = {
+                            preview_width = 0.55,
+                            results_width = 0.8,
+                        },
+                        vertical = {
+                            mirror = false,
+                        },
+                        width = 0.87,
+                        height = 0.80,
+                        preview_cutoff = 120,
+                    },
+                    file_sorter = require("telescope.sorters").get_fuzzy_file,
+                    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+                    file_ignore_patterns = { "node_modules" },
+                    path_display = { "truncate" },
+                    mappings = {
+                        n = { ["q"] = require("telescope.actions").close },
+                    },
+                }
+            }
+        end
     }
 }
