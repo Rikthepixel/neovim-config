679cf26a871275355afd8949f781b50cd9be55ee
RikThePixel <rikdenbreejen@tutanota.com>
2024-02-24 15:09:36 +0100

    Add comment regarding addition of conflict helper

--- modules/git.lua
+++ modules/git.lua
@@ -3,6 +3,10 @@ return {
         "lewis6991/gitsigns.nvim",
         opts = {},
         event = "BufEnter"
-        -- git diff --name-only --diff-filter=U --relative
     }
+    -- 
+    -- TODO: Make have a plugin or make something that gives a list of conflicts, displays them, and allows me to go through them progressively
+    --
+    -- git diff --name-only --diff-filter=U --relative
+    --
 }
