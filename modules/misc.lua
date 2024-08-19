5f366aa340e89ef58e902b50cb0a0fec97e0fedb
RikThePixel <rikdenbreejen@tutanota.com>
2024-08-18 22:36:47 -0400

    Add auto-save, cuz doing :wa with one hand gets cumbersome

---
+++ modules/misc.lua
@@ -0,0 +1,8 @@
+-- Note to self, only include plugins that really don't fall under any other category
+return {
+    {
+        "Pocco81/auto-save.nvim",
+		event = "VeryLazy",
+        opts = { }
+    }
+}
